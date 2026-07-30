0651a5958986704bba50b9709e7b6facbe95e1eb
GitHub Action <action@github.com>
2026-07-30 09:48:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/acecode0/localdictionary/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/acecode0/localdictionary/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.acecode0.localdictionary.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
